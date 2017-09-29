f6f83cf91787fd4143c6b35359779e71752ef543
Franklyn Thompson <frankistone28@outlook.com>
2017-09-29 14:54:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-    if (cardNumber === "") {
+    if (!cardNumber) {
       console.log("Sorry, we don't have a credit card on file for you.");
     }
 
